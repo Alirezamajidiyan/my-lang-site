2090fd3223bc102b25570f00cc92f96759bffb79
Alirezamajidiyan <alirezadeveloperman@gmail.com>
2025-02-02 14:14:19 +0330

    update not found page

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@ export default function NotFound() {
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-red-500">404</h1>
-        <p className="text-lg text-gray-700">صفحه مورد نظر پیدا نشد.</p>
+        <p className="text-lg text-gray-700">Page Not Found</p>
       </div>
     </div>
   );
